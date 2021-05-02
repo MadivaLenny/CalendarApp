a591b9c9df2f6ea697c14fbf09dd774a6e24a11e
Madiva Leonard <madivalenny@gmail.com>
2021-05-02 10:18:24 +0300

    1

---
+++ Calendar2/app/src/main/java/com/example/calendar/MainActivity.java
@@ -0,0 +1,31 @@
+package com.example.calendar;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.CalendarView;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    CalendarView calendarView;
+    TextView myDate;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        calendarView = (CalendarView) findViewById(R.id.calendarView);
+        myDate = (TextView) findViewById(R.id.myDate);
+
+        calendarView.setOnDateChangeListener(new CalendarView.OnDateChangeListener() {
+            @Override
+            public void onSelectedDayChange(@NonNull CalendarView calendarView, int i, int i1, int i2) {
+                String date = (i1 +1) + "/" + i2 + "/" +i;
+                myDate.setText(date);
+            }
+        });
+    }
+}
